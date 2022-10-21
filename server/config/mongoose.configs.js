6e77ac81c2f508446cdee5d64da407cc9d23dbfe
Jong Lee <76450939+Jojo970@users.noreply.github.com>
2022-10-20 23:21:01 -0500

    Delete server directory

--- server/config/mongoose.configs.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const mongoEndpoint = 'mongodb://localhost/';
-const db = 'cryptowatcher';
-
-mongoose
-    .connect(mongoEndpoint+db)
-    .then(() => console.log('Connection to Database Established'))
-    .catch((err) => console.log('Error in connecting to database', err));
